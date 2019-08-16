96f38bc82b2f9d8064d84571bcb0b5a76252daac
Benjamin J.Campbell <benjamin.campbell@usda.gov>
2019-08-16 14:17:29 -0400

    disable detail views when item not selected

---
+++ src/NatCruise.Wpf/Converters/IsNullConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NatCruise.Wpf.Converters
+{
+    [ValueConversion(typeof(object), typeof(bool))]
+    public class IsNullConverter : IValueConverter
+    {
+        public bool Invert { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isNull = value == null;
+            return isNull ^ Invert;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
