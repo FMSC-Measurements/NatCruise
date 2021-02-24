8c59139f4891a96f485db5cc485ba61f3fa9eca0
Benjamin J.Campbell <benjamin.campbell@usda.gov>
2021-02-24 16:38:35 -0500

    add reset database to settings page

--- src/FScruiser.Xamarin/ViewModels/SettingsViewModel.cs
+++ src/FScruiser.Xamarin/ViewModels/SettingsViewModel.cs
@@ -3,19 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using CruiseDAL;
 using FScruiser.XF.Data;
 using FScruiser.XF.Services;
+using NatCruise.Services;
 using Prism.Navigation;
+using Xamarin.Forms;
 
 namespace FScruiser.XF.ViewModels
 {
     public class SettingsViewModel : ViewModelBase
     {
-        public IApplicationSettings AppSettings { get; set; }
+        public IApplicationSettings AppSettings { get; }
+        public IDialogService DialogService { get; }
+        public IFileSystemService FileSystemService { get; }
 
-        public SettingsViewModel()
+        public ICommand ResetDatabaseCommand => new Command(() => ResetDatabase());
+
+        public SettingsViewModel(IDialogService dialogService, IFileSystemService fileSystemService)
         {
             AppSettings = new ApplicationSettings();
+            DialogService = dialogService ?? throw new ArgumentNullException(nameof(dialogService));
+            FileSystemService = fileSystemService ?? throw new ArgumentNullException(nameof(fileSystemService));
+        }
+
+        public async void ResetDatabase()
+        {
+            if (await DialogService.AskYesNoAsync("This will delete all cruise data do you want to continue", "Warning", defaultNo: true))
+            {
+                var databasePath = FileSystemService.DefaultCruiseDatabasePath;
+                var newDatabase = new CruiseDatastore_V3(databasePath, true);
+            }
         }
 
         protected override void Refresh(INavigationParameters parameters)
