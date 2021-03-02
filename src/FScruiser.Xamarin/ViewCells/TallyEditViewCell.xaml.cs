ee6bd12fd41c43686d2dddbc96dfd9a232dc8188
Ben Campbell <benjocamps@gmail.com>
2021-03-02 09:58:33 -0500

    update tally view pt2

--- src/FScruiser.Xamarin/ViewCells/TallyEditViewCell.xaml.cs
+++ src/FScruiser.Xamarin/ViewCells/TallyEditViewCell.xaml.cs
@@ -10,7 +10,7 @@ using Xamarin.Forms.Xaml;
 namespace FScruiser.XF.ViewCells
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class TallyEditViewCell : ViewCell
+	public partial class TallyEditViewCell : ContentView
 	{
 		public TallyEditViewCell ()
 		{
--- src/FScruiser.Xamarin/ViewCells/TallyEntryViewCell.xaml.cs
+++ src/FScruiser.Xamarin/ViewCells/TallyEntryViewCell.xaml.cs
@@ -7,24 +7,12 @@ using Xamarin.Forms.Xaml;
 namespace FScruiser.XF.ViewCells
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class TallyEntryViewCell : TallyEntryViewCell_Base
+    public partial class TallyEntryViewCell : ContentView
     {
 
         public TallyEntryViewCell()
         {
             InitializeComponent();
-
-            _untallyButton.Clicked += _untallyButton_Clicked;
-        }
-
-        protected override View DrawrView => _drawer;
-
-        protected void _untallyButton_Clicked(object sender, EventArgs e)
-        {
-            if (BindingContext is TallyEntry tallyEntry && tallyEntry != null)
-            {
-                UntallyCommand?.Execute(tallyEntry.TallyLedgerID);
-            }
         }
     }
 }
