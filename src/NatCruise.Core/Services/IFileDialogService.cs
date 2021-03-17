77d975ee1ebccc1e7a2f75e93a8aef32cc94fa7c
Benjamin J.Campbell <benjamin.campbell@usda.gov>
2021-03-17 15:14:18 -0400

    add SelectCruiseFileDestinatinAsyn back to IFileDialogService

--- src/NatCruise.Core/Services/IFileDialogService.cs
+++ src/NatCruise.Core/Services/IFileDialogService.cs
@@ -5,5 +5,7 @@ namespace NatCruise.Services
     public interface IFileDialogService
     {
         Task<string> SelectCruiseFileAsync();
+
+        Task<string> SelectCruiseFileDestinationAsync(string defaultDir = null, string defaultFileName = null);
     }
 }
