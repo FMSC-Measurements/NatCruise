ba7a5d02e1d1b937e0ad89d3d4c5885b539d6478
Benjamin J.Campbell <benjamin.campbell@usda.gov>
2021-03-23 15:00:42 -0400

    Update XamarinViewModelBase.cs

--- src/FScruiser.Xamarin/ViewModels/XamarinViewModelBase.cs
+++ src/FScruiser.Xamarin/ViewModels/XamarinViewModelBase.cs
@@ -19,50 +19,6 @@ namespace FScruiser.XF.ViewModels
                 Parameters = parameters;
                 _isFirstNavigatedTo = false;
             }
-            else
-            {
-                throw new Exception(); // can Initialize get called multiple times?
-            }
         }
-
-        //public virtual void OnNavigatedFrom(INavigationParameters parameters)
-        //{
-        //}
-
-        //public virtual void OnNavigatedTo(INavigationParameters parameters)
-        //{
-        //    if (_isFirstNavigatedTo)
-        //    {
-        //        Parameters = parameters;
-        //        _isFirstNavigatedTo = false;
-        //    }
-
-        //    try
-        //    {
-        //        var stopwatch = Stopwatch.StartNew();
-
-        //        Refresh();
-
-        //        stopwatch.Stop();
-        //        Microsoft.AppCenter.Analytics.Analytics.TrackEvent("view_model_refresh",
-        //            new Dictionary<string, string> {
-        //                { "time_ms", stopwatch.ElapsedMilliseconds.ToString() },
-        //                { "view_model_type", this.GetType().Name },
-        //            });
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Debug.WriteLine("ERROR::::" + ex);
-        //        Crashes.TrackError(ex, new Dictionary<string, string>() { { "view_model_type", this.GetType().Name } });
-        //    }
-
-        //}
-
-        //public void Refresh()
-        //{
-        //    Refresh(Parameters);
-        //}
-
-        //protected abstract void Refresh(IParameters parameters);
     }
 }
