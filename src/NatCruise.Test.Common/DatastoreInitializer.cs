4dc5fd77e92fedd517b8b22ccbc5a3ab92616101
Ben Campbell <benjocamps@gmail.com>
2021-03-25 15:52:02 -0400

    initialize deviceID in DatastoreInisializer

--- src/NatCruise.Test.Common/DatastoreInitializer.cs
+++ src/NatCruise.Test.Common/DatastoreInitializer.cs
@@ -11,6 +11,7 @@ namespace NatCruise.Test
         public string CruiseID { get; }
         public string SaleID { get; }
 
+        public string DeviceID { get; }
         public string[] Units { get; }
         public Stratum[] Strata { get; }
         public CuttingUnit_Stratum[] UnitStrata { get; }
@@ -25,6 +26,7 @@ namespace NatCruise.Test
         {
             CruiseID = Guid.NewGuid().ToString();
             SaleID = Guid.NewGuid().ToString();
+            DeviceID = Guid.NewGuid().ToString();
 
             var units = Units = new string[] { "u1", "u2" };
 
@@ -125,54 +127,27 @@ namespace NatCruise.Test
             var cruiseID = CruiseID;
             var saleID = SaleID;
 
-            var units = Units;
-
-            var strata = Strata;
-
-            var unitStrata = UnitStrata;
-
-            var sampleGroups = SampleGroups;
-
-            var species = Species;
-
-            var tdvs = TreeDefaults;
-
-            var subPops = Subpops;
-
             var database = new CruiseDatastore_V3();
 
-            InitializeDatabase(database, cruiseID, saleID, units, strata, unitStrata, sampleGroups, species, tdvs, subPops);
+            InitializeDatabase(database, DeviceID, cruiseID, saleID, Units, Strata, UnitStrata, SampleGroups, Species, TreeDefaults, Subpops);
 
             return database;
         }
 
         public CruiseDatastore_V3 CreateDatabase(string path, string cruiseID = null, string saleID = null)
         {
-            cruiseID = cruiseID ?? Guid.NewGuid().ToString();
-            saleID = saleID ?? Guid.NewGuid().ToString();
-
-            var units = Units;
-
-            var strata = Strata;
-
-            var unitStrata = UnitStrata;
-
-            var sampleGroups = SampleGroups;
-
-            var species = Species;
-
-            var tdvs = TreeDefaults;
-
-            var subPops = Subpops;
+            cruiseID = cruiseID ?? CruiseID;
+            saleID = saleID ?? SaleID;
 
             var database = new CruiseDatastore_V3(path, true);
 
-            InitializeDatabase(database, cruiseID, saleID, units, strata, unitStrata, sampleGroups, species, tdvs, subPops);
+            InitializeDatabase(database, DeviceID, cruiseID, saleID, Units, Strata, UnitStrata, SampleGroups, Species, TreeDefaults, Subpops);
 
             return database;
         }
 
         public static void InitializeDatabase(CruiseDatastore_V3 db,
+    string deviceID,
     string cruiseID,
     string saleID,
     string[] units,
@@ -180,20 +155,29 @@ namespace NatCruise.Test
     CruiseDAL.V3.Models.CuttingUnit_Stratum[] unitStrata,
     CruiseDAL.V3.Models.SampleGroup[] sampleGroups,
     string[] species,
-    CruiseDAL.V3.Models.TreeDefaultValue[] tdvs,
-    CruiseDAL.V3.Models.SubPopulation[] subPops)
+    CruiseDAL.V3.Models.TreeDefaultValue[] tdvs, CruiseDAL.V3.Models.SubPopulation[] subPops)
         {
+            var saleNumber = (saleID.GetHashCode() % 10000).ToString();
+
             db.Insert(new Sale()
             {
                 SaleID = saleID,
-                SaleNumber = (saleID.GetHashCode() % 10000).ToString(),
-            }, option: Backpack.SqlBuilder.OnConflictOption.Ignore); ;
+                SaleNumber = saleNumber,
+            }); ;
 
             db.Insert(new CruiseDAL.V3.Models.Cruise()
             {
                 CruiseID = cruiseID,
                 SaleID = saleID,
-            }, option: Backpack.SqlBuilder.OnConflictOption.Ignore);
+                CruiseNumber = saleNumber,
+            });
+
+            db.Insert(new Device()
+            {
+                CruiseID = cruiseID,
+                DeviceID = deviceID,
+                Name = "deviceName",
+            });
 
             //Cutting Units
             foreach (var unit in units.OrEmpty())
