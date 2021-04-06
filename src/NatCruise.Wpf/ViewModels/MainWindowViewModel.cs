3df374c94edd3f7a7932abf8401bbf8cf0d71deb
Ben Campbell <benjocamps@gmail.com>
2021-04-06 12:28:51 -0400

    update OpenFile

--- src/NatCruise.Wpf/ViewModels/MainWindowViewModel.cs
+++ src/NatCruise.Wpf/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using NatCruise.Core.Services;
 using NatCruise.Data;
 using NatCruise.Design.Services;
+using NatCruise.Models;
 using NatCruise.Services;
 using Prism.Commands;
 using Prism.Ioc;
@@ -26,6 +27,7 @@ namespace NatCruise.Wpf.ViewModels
             IFileDialogService fileDialogService,
             IRecentFilesDataservice recentFilesDataservice,
             Prism.Services.Dialogs.IDialogService dialogService,
+            NatCruise.Services.IDialogService cruiseDialogService,
             IDeviceInfoService deviceInfo)
         {
             DataserviceProvider = dataserviceProvider ?? throw new ArgumentNullException(nameof(dataserviceProvider));
@@ -34,18 +36,20 @@ namespace NatCruise.Wpf.ViewModels
             RecentFilesDataservice = recentFilesDataservice ?? throw new ArgumentNullException(nameof(recentFilesDataservice));
             NavigationService = navigationService ?? throw new ArgumentNullException(nameof(navigationService));
             DeviceInfoService = deviceInfo ?? throw new ArgumentNullException(nameof(deviceInfo));
+            CruiseDialogService = cruiseDialogService ?? throw new ArgumentNullException(nameof(cruiseDialogService));
         }
 
         protected IDataserviceProvider DataserviceProvider { get; }
         protected IDesignNavigationService NavigationService { get; }
         protected IRecentFilesDataservice RecentFilesDataservice { get; }
         public Prism.Services.Dialogs.IDialogService DialogService { get; }
+        protected NatCruise.Services.IDialogService CruiseDialogService { get; }
         protected IFileDialogService FileDialogService { get; }
         protected IDeviceInfoService DeviceInfoService { get; }
 
         //protected IRegionNavigationService RegionNavigationService => RegionManager?.Regions[Regions.ContentRegion].NavigationService;
 
-        public string Title => "National Cruise System (0.2.1-Alpha)";
+        public string Title => "National Cruise System (0.3)";
 
         public IEnumerable<FileInfo> RecentFiles => RecentFilesDataservice?.GetRecentFiles().Select(x => new FileInfo(x));
 
@@ -94,7 +98,17 @@ namespace NatCruise.Wpf.ViewModels
         {
             var filePath = file.FullName;
             var database = new CruiseDatastore_V3(filePath);
+
+            var cruiseIDs = database.QueryScalar<string>("SELECT CruiseID FROM Cruise;").ToArray();
+            if (cruiseIDs.Length > 1)
+            {
+                CruiseDialogService.ShowNotification("File contains multiple cruises. \r\nOpening files with multiple cruises is not supported yet", "Warning");
+                return;
+            }
+            var cruiseID = cruiseIDs.First();
+
             DataserviceProvider.Database = database;
+            DataserviceProvider.CruiseID = cruiseID;
 
             NavigationService.ShowCruiseLandingLayout();
             RecentFilesDataservice.AddRecentFile(filePath);
