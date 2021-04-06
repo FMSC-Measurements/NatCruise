e7862d9d4ae2cc03c82329656b3e6112c17eb082
Benjamin J.Campbell <benjamin.campbell@usda.gov>
2021-04-06 12:12:15 -0400

    cruise view and viewmodel

---
+++ src/NatCruise.Design/ViewModels/CruiseViewModel.cs
@@ -0,0 +1,76 @@
+﻿using NatCruise.Data;
+using NatCruise.Data.Abstractions;
+using NatCruise.Design.Data;
+using NatCruise.Design.Models;
+using NatCruise.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace NatCruise.Design.ViewModels
+{
+    public class CruiseViewModel : ViewModelBase
+    {
+        private Cruise _cruise;
+        private IEnumerable<Purpose> _purposeOptions;
+
+        public CruiseViewModel(IDataserviceProvider dataserviceProvider, ISetupInfoDataservice setupInfo)
+        {
+            if (dataserviceProvider is null) { throw new ArgumentNullException(nameof(dataserviceProvider)); }
+
+            SaleDataservice = dataserviceProvider.GetDataservice<ISaleDataservice>();
+            SetupDataservice = setupInfo ?? throw new ArgumentNullException(nameof(setupInfo));
+        }
+
+        private ISaleDataservice SaleDataservice { get; }
+
+        protected ISetupInfoDataservice SetupDataservice { get; }
+
+        public Cruise Cruise
+        {
+            get => _cruise;
+            set
+            {
+                OnCruiseChanging(_cruise, value);
+                SetProperty(ref _cruise, value);
+                OnCruiseChanged(value);
+            }
+        }
+
+        private void OnCruiseChanged(Cruise newValue)
+        {
+            if (newValue != null)
+            {
+                newValue.PropertyChanged += Cruise_PropertyChanged;
+            }
+        }
+
+        private void OnCruiseChanging(Cruise oldValue, Cruise newValue)
+        {
+            if (oldValue != null)
+            {
+                oldValue.PropertyChanged -= Cruise_PropertyChanged;
+            }
+        }
+
+        private void Cruise_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var cruise = sender as Cruise;
+            SaleDataservice.UpdateCruise(cruise);
+        }
+
+        public IEnumerable<Purpose> PurposeOptions
+        {
+            get => _purposeOptions;
+            set => SetProperty(ref _purposeOptions, value);
+        }
+
+        public override void Load()
+        {
+            base.Load();
+
+            Cruise = SaleDataservice.GetCruise();
+            PurposeOptions = SetupDataservice.GetPurposes();
+        }
+    }
+}
