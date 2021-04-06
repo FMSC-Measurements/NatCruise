ca82beee9a943f9fa19c35f06e667baf4dfa9895
Benjamin J.Campbell <benjamin.campbell@usda.gov>
2021-04-06 12:12:49 -0400

    add value converters

---
+++ src/NatCruise.Wpf/Converters/IsVisableConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NatCruise.Wpf.Converters
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class IsVisableConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var isVisable = (bool)value;
+                return (isVisable) ? Visibility.Visible : Visibility.Collapsed;
+            }
+            catch
+            { return true; }
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
