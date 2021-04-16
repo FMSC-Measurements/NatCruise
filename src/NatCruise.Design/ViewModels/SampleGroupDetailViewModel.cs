d1cb4fc05b239e8d62e4ae276a45a28f6437e2f4
Benjamin J.Campbell <benjamin.campbell@usda.gov>
2021-04-16 13:13:58 -0400

    fix productOptions, fix prop changed notifications on Sg changed

--- src/NatCruise.Design/ViewModels/SampleGroupDetailViewModel.cs
+++ src/NatCruise.Design/ViewModels/SampleGroupDetailViewModel.cs
@@ -10,15 +10,22 @@ namespace NatCruise.Design.ViewModels
 {
     public class SampleGroupDetailViewModel : ViewModelBase
     {
+        protected readonly string[] Frequency_Based_CruiseMethods = new[] {CruiseMethods.STR, CruiseMethods.PCM, CruiseMethods.FCM, CruiseMethods.S3P, };
+        protected readonly string[] SampleSelector_Type_Options = new[] { CruiseMethods.BLOCK_SAMPLER_TYPE, CruiseMethods.SYSTEMATIC_SAMPLER_TYPE, };
+        protected readonly string[] Supports_SamplerType_Selection = new[] { CruiseMethods.STR, CruiseMethods.PCM, CruiseMethods.FCM, }; 
+
         private SampleGroup _sampleGroup;
         private string _method;
         private IEnumerable<string> _sampleSelectorTypeOptions;
+        private IEnumerable<Product> _productOptions;
 
         public SampleGroupDetailViewModel(IDataserviceProvider dataserviceProvider, ISetupInfoDataservice setupInfo)
         {
             var sampleGroupDataservice = dataserviceProvider.GetDataservice<ISampleGroupDataservice>();
             SampleGroupDataservice = sampleGroupDataservice ?? throw new ArgumentNullException(nameof(sampleGroupDataservice));
             SetupDataservice = setupInfo ?? throw new ArgumentNullException(nameof(setupInfo));
+
+            ProductOptions = SetupDataservice.GetProducts();
         }
 
         public ISampleGroupDataservice SampleGroupDataservice { get; }
@@ -26,7 +33,11 @@ namespace NatCruise.Design.ViewModels
 
         public IEnumerable<string> DefaultLiveDeadOptions { get; } = new[] { "L", "D" };
 
-        public IEnumerable<Product> ProductOptions { get; protected set; }
+        public IEnumerable<Product> ProductOptions
+        {
+            get => _productOptions;
+            protected set => SetProperty(ref _productOptions, value);
+        }
 
         public SampleGroup SampleGroup
         {
@@ -36,6 +47,14 @@ namespace NatCruise.Design.ViewModels
                 OnSampleGroupChanging(_sampleGroup);
                 SetProperty(ref _sampleGroup, value);
                 OnSampleGroupChanged(value);
+
+                RaisePropertyChanged(nameof(SampleSelectorType));
+                RaisePropertyChanged(nameof(IsSTR));
+                RaisePropertyChanged(nameof(Is3P));
+                RaisePropertyChanged(nameof(IsVariableRadious));
+                RaisePropertyChanged(nameof(CanSelectSampleSelectorType));
+                RaisePropertyChanged(nameof(DefaultSampleSelectorType));
+                RaisePropertyChanged(nameof(IsFrequencyBased));
             }
         }
 
@@ -46,7 +65,11 @@ namespace NatCruise.Design.ViewModels
 
         private void OnSampleGroupChanged(SampleGroup sampleGroup)
         {
-            if (sampleGroup == null) { return; }
+            if (sampleGroup == null)
+            {
+                Method = null;
+                return;
+            }
 
             Method = SampleGroupDataservice.GetMethod(sampleGroup.StratumCode);
             sampleGroup.PropertyChanged += SampleGroup_PropertyChanged;
@@ -64,21 +87,44 @@ namespace NatCruise.Design.ViewModels
             protected set
             {
                 SetProperty(ref _method, value);
-                RaisePropertyChanged(nameof(IsSTR));
-                RaisePropertyChanged(nameof(Is3P));
-                RaisePropertyChanged(nameof(IsVariableRadious));
             }
         }
 
         public bool IsSTR => Method == CruiseMethods.STR;
-        public bool Is3P => CruiseMethods.THREE_P_METHODS.Contains(Method);
-        public IEnumerable<string> SampleSelectorTypeOptions
-        {
-            get => _sampleSelectorTypeOptions;
-            set => SetProperty(ref _sampleSelectorTypeOptions, value);
-        }
+        public bool IsFrequencyBased => Frequency_Based_CruiseMethods.Contains(Method);
+        public bool Is3P => CruiseMethods.THREE_P_METHODS.Contains(Method) || Method == CruiseMethods.S3P;
+        //public IEnumerable<string> SampleSelectorTypeOptions
+        //{
+        //    get => _sampleSelectorTypeOptions;
+        //    set => SetProperty(ref _sampleSelectorTypeOptions, value);
+        //}
 
         public bool IsVariableRadious => CruiseMethods.VARIABLE_RADIUS_METHODS.Contains(Method);
+
+        public IEnumerable<string> SampleSelectorTypeOptions => SampleSelector_Type_Options;
+
+        public string DefaultSampleSelectorType
+        {
+            get
+            {
+                if(Method == CruiseMethods.STR)
+                { return CruiseMethods.BLOCK_SAMPLER_TYPE; }
+                else if (Method == CruiseMethods.PCM || Method == CruiseMethods.FCM)
+                { return CruiseMethods.SYSTEMATIC_SAMPLER_TYPE; }
+                return null;
+            }
+        }
+
+        public bool CanSelectSampleSelectorType => Supports_SamplerType_Selection.Contains(Method);
+
+        public string SampleSelectorType
+        {
+            get => SampleGroup?.SampleSelectorType ?? DefaultSampleSelectorType;
+            set
+            {
+                SampleGroup.SampleSelectorType = value;
+            }
+        }
 
         //protected override void Refresh(CruiseManagerNavigationParamiters navParams)
         //{
@@ -98,25 +144,25 @@ namespace NatCruise.Design.ViewModels
             var sampleGroup = SampleGroup;
             if (sampleGroup == null) { return; }
 
-            ProductOptions = SetupDataservice.GetProducts();
+            
             var method = SampleGroupDataservice.GetMethod(sampleGroup.StratumCode);
             Method = method;
 
-            switch(method)
-            {
-                case CruiseMethods.STR:
-                case CruiseMethods.PCM:
-                case CruiseMethods.FCM:
-                    {
-                        SampleSelectorTypeOptions = new[]
-                          {
-                            SampleGroupTableDefinition.SAMPLESELECTORTYPE_BLOCKSELECTER,
-                            SampleGroupTableDefinition.SAMPLESELECTORTYPE_SYSTEMATICSELECTER,
-                            SampleGroupTableDefinition.SAMPLESELECTORTYPE_CLICKERSELECTER
-                        };
-                        break;
-                    }
-            }
+            //switch (method)
+            //{
+            //    case CruiseMethods.STR:
+            //    case CruiseMethods.PCM:
+            //    case CruiseMethods.FCM:
+            //        {
+            //            SampleSelectorTypeOptions = new[]
+            //              {
+            //                SampleGroupTableDefinition.SAMPLESELECTORTYPE_BLOCKSELECTER,
+            //                SampleGroupTableDefinition.SAMPLESELECTORTYPE_SYSTEMATICSELECTER,
+            //                SampleGroupTableDefinition.SAMPLESELECTORTYPE_CLICKERSELECTER
+            //            };
+            //            break;
+            //        }
+            //}
         }
     }
 }
