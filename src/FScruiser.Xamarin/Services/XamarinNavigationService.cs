2e1fc020189cb496ad1581b8630b440029df1639
Benjamin J.Campbell <benjamin.campbell@usda.gov>
2021-04-22 16:16:52 -0400

    add null/empty checks for params

--- src/FScruiser.Xamarin/Services/XamarinNavigationService.cs
+++ src/FScruiser.Xamarin/Services/XamarinNavigationService.cs
@@ -44,12 +44,22 @@ namespace FScruiser.XF.Services
 
         public Task ShowCruiseSelect(string saleID)
         {
+            if (string.IsNullOrEmpty(saleID))
+            {
+                throw new ArgumentException($"'{nameof(saleID)}' cannot be null or empty.", nameof(saleID));
+            }
+
             return NavigationService.NavigateAsync("CruiseSelect",
                 new NavigationParameters($"{NavParams.SaleID}={saleID}"));
         }
 
         public Task ShowCuttingUnitInfo(string unitCode)
         {
+            if (string.IsNullOrEmpty(unitCode))
+            {
+                throw new ArgumentException($"'{nameof(unitCode)}' cannot be null or empty.", nameof(unitCode));
+            }
+
             return NavigationService.NavigateAsync("/Main/Navigation/CuttingUnitInfo",
                 new NavigationParameters($"{NavParams.UNIT}={unitCode}"));
         }
@@ -67,6 +77,16 @@ namespace FScruiser.XF.Services
 
         public Task ShowFixCNT(string unitCode, int plotNumber, string stratumCode)
         {
+            if (string.IsNullOrEmpty(unitCode))
+            {
+                throw new ArgumentException($"'{nameof(unitCode)}' cannot be null or empty.", nameof(unitCode));
+            }
+
+            if (string.IsNullOrEmpty(stratumCode))
+            {
+                throw new ArgumentException($"'{nameof(stratumCode)}' cannot be null or empty.", nameof(stratumCode));
+            }
+
             return NavigationService.NavigateAsync($"FixCNT",
                 new NavigationParameters($"{NavParams.UNIT}={unitCode}&{NavParams.PLOT_NUMBER}={plotNumber}&{NavParams.STRATUM}={stratumCode}"));
         }
@@ -100,12 +120,22 @@ namespace FScruiser.XF.Services
 
         public Task ShowLogEdit(string logID)
         {
+            if (string.IsNullOrEmpty(logID))
+            {
+                throw new ArgumentException($"'{nameof(logID)}' cannot be null or empty.", nameof(logID));
+            }
+
             return NavigationService.NavigateAsync("LogEdit",
                 new NavigationParameters($"{NavParams.LogID}={logID}"));
         }
 
         public async Task ShowLogsList(string treeID)
         {
+            if (string.IsNullOrEmpty(treeID))
+            {
+                throw new ArgumentException($"'{nameof(treeID)}' cannot be null or empty.", nameof(treeID));
+            }
+
             try
             {
                 var result = await NavigationService.NavigateAsync("LogList",
@@ -125,36 +155,56 @@ namespace FScruiser.XF.Services
 
         public Task ShowPlotEdit(string plotID)
         {
+            if (string.IsNullOrEmpty(plotID))
+            {
+                throw new ArgumentException($"'{nameof(plotID)}' cannot be null or empty.", nameof(plotID));
+            }
+
             return NavigationService.NavigateAsync("PlotEdit",
                 new NavigationParameters($"{NavParams.PlotID}={plotID}"));
         }
 
-        public Task ShowPlotEdit(string unitCode, int plotNumber)
-        {
-            return NavigationService.NavigateAsync("PlotEdit",
-                new NavigationParameters($"{NavParams.UNIT}={unitCode}&{NavParams.PLOT_NUMBER}={plotNumber}"));
-        }
+        //public Task ShowPlotEdit(string unitCode, int plotNumber)
+        //{
+        //    return NavigationService.NavigateAsync("PlotEdit",
+        //        new NavigationParameters($"{NavParams.UNIT}={unitCode}&{NavParams.PLOT_NUMBER}={plotNumber}"));
+        //}
 
         public Task ShowPlotList(string unitCode)
         {
+            if (string.IsNullOrEmpty(unitCode))
+            {
+                throw new ArgumentException($"'{nameof(unitCode)}' cannot be null or empty.", nameof(unitCode));
+            }
+
             return NavigationService.NavigateAsync("/Main/Navigation/PlotList",
                 new NavigationParameters($"{NavParams.UNIT}={unitCode}"));
         }
 
         public Task ShowPlotTally(string plotID)
         {
+            if (string.IsNullOrEmpty(plotID))
+            {
+                throw new ArgumentException($"'{nameof(plotID)}' cannot be null or empty.", nameof(plotID));
+            }
+
             return NavigationService.NavigateAsync("PlotTally",
                 new NavigationParameters($"{NavParams.PlotID}={plotID}"));
         }
 
-        public Task ShowPlotTally(string unitCode, int plotNumber)
-        {
-            return NavigationService.NavigateAsync("PlotTally",
-                new NavigationParameters($"{NavParams.UNIT}={unitCode}&{NavParams.PLOT_NUMBER}={plotNumber}"));
-        }
+        //public Task ShowPlotTally(string unitCode, int plotNumber)
+        //{
+        //    return NavigationService.NavigateAsync("PlotTally",
+        //        new NavigationParameters($"{NavParams.UNIT}={unitCode}&{NavParams.PLOT_NUMBER}={plotNumber}"));
+        //}
 
         public Task ShowSale(string cruiseID)
         {
+            if (string.IsNullOrEmpty(cruiseID))
+            {
+                throw new ArgumentException($"'{nameof(cruiseID)}' cannot be null or empty.", nameof(cruiseID));
+            }
+
             return NavigationService.NavigateAsync("/Main/Navigation/Sale",
                 new NavigationParameters($"{NavParams.CruiseID}={cruiseID}"));
         }
@@ -176,24 +226,59 @@ namespace FScruiser.XF.Services
 
         public Task ShowTally(string unitCode)
         {
+            if (string.IsNullOrEmpty(unitCode))
+            {
+                throw new ArgumentException($"'{nameof(unitCode)}' cannot be null or empty.", nameof(unitCode));
+            }
+
             return NavigationService.NavigateAsync("/Main/Navigation/Tally",
                 new NavigationParameters($"{NavParams.UNIT}={unitCode}"));
         }
 
         public Task ShowThreePPNTPlot(string unitCode, string stratumCode, int plotNumber)
         {
+            if (string.IsNullOrEmpty(unitCode))
+            {
+                throw new ArgumentException($"'{nameof(unitCode)}' cannot be null or empty.", nameof(unitCode));
+            }
+
+            if (string.IsNullOrEmpty(stratumCode))
+            {
+                throw new ArgumentException($"'{nameof(stratumCode)}' cannot be null or empty.", nameof(stratumCode));
+            }
+
             return NavigationService.NavigateAsync("ThreePPNTPlot",
                 new NavigationParameters($"{NavParams.UNIT}={unitCode}&{NavParams.STRATUM}={stratumCode}&{NavParams.PLOT_NUMBER}={plotNumber}"));
         }
 
         public Task ShowTreeCountEdit(string unitCode, string stratumCode, string sampleGroupCode, string species, string liveDead)
         {
+            if (string.IsNullOrEmpty(unitCode))
+            {
+                throw new ArgumentException($"'{nameof(unitCode)}' cannot be null or empty.", nameof(unitCode));
+            }
+
+            if (string.IsNullOrEmpty(stratumCode))
+            {
+                throw new ArgumentException($"'{nameof(stratumCode)}' cannot be null or empty.", nameof(stratumCode));
+            }
+
+            if (string.IsNullOrEmpty(sampleGroupCode))
+            {
+                throw new ArgumentException($"'{nameof(sampleGroupCode)}' cannot be null or empty.", nameof(sampleGroupCode));
+            }
+
             return NavigationService.NavigateAsync("TreeCountEdit",
                 new NavigationParameters($"{NavParams.UNIT}={unitCode}&{NavParams.STRATUM}={stratumCode}&{NavParams.SAMPLE_GROUP}={sampleGroupCode}&{NavParams.SPECIES}={species}&{NavParams.LIVE_DEAD}={liveDead}"));
         }
 
         public async Task ShowTreeEdit(string treeID)
         {
+            if (string.IsNullOrEmpty(treeID))
+            {
+                throw new ArgumentException($"'{nameof(treeID)}' cannot be null or empty.", nameof(treeID));
+            }
+
             try
             {
                 var result = await NavigationService.NavigateAsync("Tree",
@@ -213,12 +298,27 @@ namespace FScruiser.XF.Services
 
         public Task ShowTreeErrorEdit(string treeID, string treeAuditRuleID)
         {
+            if (string.IsNullOrEmpty(treeID))
+            {
+                throw new ArgumentException($"'{nameof(treeID)}' cannot be null or empty.", nameof(treeID));
+            }
+
+            if (string.IsNullOrEmpty(treeAuditRuleID))
+            {
+                throw new ArgumentException($"'{nameof(treeAuditRuleID)}' cannot be null or empty.", nameof(treeAuditRuleID));
+            }
+
             return NavigationService.NavigateAsync("TreeErrorEdit",
                 new NavigationParameters($"{NavParams.TreeID}={treeID}&{NavParams.TreeAuditRuleID}={treeAuditRuleID}"));
         }
 
         public Task ShowTreeList(string unitCode)
         {
+            if (string.IsNullOrEmpty(unitCode))
+            {
+                throw new ArgumentException($"'{nameof(unitCode)}' cannot be null or empty.", nameof(unitCode));
+            }
+
             return NavigationService.NavigateAsync("/Main/Navigation/TreeList",
                 new NavigationParameters($"{NavParams.UNIT}={unitCode}"));
         }
