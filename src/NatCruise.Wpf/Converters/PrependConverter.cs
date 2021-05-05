8da41c434ce3d716893e1fe627dcc79639dbea02
Ben Campbell <benjocamps@gmail.com>
2021-05-05 17:01:19 -0400

    add none option for product codes

---
+++ src/NatCruise.Wpf/Converters/PrependConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace NatCruise.Wpf.Converters
+{
+    public class PrependConverter : IValueConverter
+    {
+        public object Value { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value == null) { return null; }
+
+            var items = (IEnumerable<object>)value;
+            return Enumerable.Prepend(items, parameter ?? Value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
