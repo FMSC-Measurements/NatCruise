fd57c878f00a1bcbe5b88523589bd6744ae3479b
Ben Campbell <benjocamps@gmail.com>
2021-05-10 18:22:35 -0400

    Bump version and update deployment scripts

---
+++ src/Build_Installer_PC.bat
@@ -0,0 +1,42 @@
+::Boilderplate 
+::@ECHO OFF
+SETLOCAL ENABLEEXTENSIONS
+
+::detect if invoked via Window Explorer
+SET interactive=1
+ECHO %CMDCMDLINE% | FIND /I "/c" >NUL 2>&1
+IF %ERRORLEVEL% == 0 SET interactive=0
+
+::name of this script
+SET me=%~n0
+::directory of script
+SET parent=%~dp0
+::::::::::::::::::
+
+
+IF NOT DEFINED verStamp (SET verStamp=%date:~10,4%%date:~4,2%%date:~7,2%)
+
+CALL %parent%/Build_Release_PC.cmd
+
+IF /I "%ERRORLEVEL%" NEQ "0" (
+ECHO build failed
+IF "%interactive%"=="0" PAUSE
+EXIT /B 1
+)
+
+SET innoSetupPath="%localappdata%\Programs\Inno Setup 6\ISCC.exe"
+IF NOT EXIST %innoSetupPath% (
+	SET innoSetupPath="%ProgramFiles(x86)%\Inno Setup 6\ISCC.exe")
+
+IF NOT EXIST %innoSetupPath% (
+	ECHO "inno setup not found"
+	IF "%interactive%"=="0" PAUSE
+	EXIT /B 1)
+
+%innoSetupPath% /dAPP_VERSION=%appVer% "./NatCruise.Wpf/Setup.iss" 
+
+::End Boilderplate
+::if invoked from windows explorer, pause
+IF "%interactive%"=="0" PAUSE
+ENDLOCAL
+EXIT /B 0
