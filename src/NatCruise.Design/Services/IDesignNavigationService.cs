8c6489c701eb3ce207c41706528eedc24b6aaa41
Benjamin J.Campbell <benjamin.campbell@usda.gov>
2021-08-30 14:32:28 -0400

    template files view/edit/create

--- src/NatCruise.Design/Services/IDesignNavigationService.cs
+++ src/NatCruise.Design/Services/IDesignNavigationService.cs
@@ -19,6 +19,8 @@ namespace NatCruise.Design.Services
 
         Task ShowCruiseLandingLayout();
 
+        Task ShowTemplateLandingLayout();
+
         Task ShowAuditRules();
 
         Task ShowTreeDefaultValues();
---
+++ src/NatCruise.Wpf/Views/TemplateMasterView.xaml.cs
@@ -0,0 +1,63 @@
+﻿using NatCruise.Design.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NatCruise.Wpf.Views
+{
+    /// <summary>
+    /// Interaction logic for TemplateMasterView.xaml
+    /// </summary>
+    public partial class TemplateMasterView : UserControl
+    {
+        public TemplateMasterView(IDesignNavigationService navigationService)
+        {
+            InitializeComponent();
+
+            NavigationService = navigationService ?? throw new ArgumentNullException(nameof(navigationService));
+        }
+
+        public IDesignNavigationService NavigationService { get; }
+
+        private void _auditRuleButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.ShowAuditRules();
+        }
+
+        private void _tdv_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.ShowTreeDefaultValues();
+        }
+
+        private void _speciesButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.ShowSpecies();
+        }
+
+        private void _designTemplatesButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.ShowDesignTemplates();
+        }
+
+        private void _treeFieldsButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.ShowTreeFields();
+        }
+
+        private void _logFieldsButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.ShowLogFields();
+        }
+    }
+}
