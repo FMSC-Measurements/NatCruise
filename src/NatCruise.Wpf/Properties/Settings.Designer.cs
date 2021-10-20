f1d20c2889fbc84f2adf3d3936e65cd67e20490a
Benjamin J.Campbell <benjamin.campbell@usda.gov>
2021-10-20 17:14:41 -0400

    remember last cruise and template file directory

--- src/NatCruise.Wpf/Properties/Settings.Designer.cs
+++ src/NatCruise.Wpf/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace NatCruise.Wpf.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -32,6 +32,30 @@ namespace NatCruise.Wpf.Properties {
             }
             set {
                 this["RecentFiles"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string LastOpenCruiseDir {
+            get {
+                return ((string)(this["LastOpenCruiseDir"]));
+            }
+            set {
+                this["LastOpenCruiseDir"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string LastOpenTemplateDir {
+            get {
+                return ((string)(this["LastOpenTemplateDir"]));
+            }
+            set {
+                this["LastOpenTemplateDir"] = value;
             }
         }
     }
---
+++ src/NatCruise.Wpf/Services/IWpfApplicationSettingService.cs
@@ -0,0 +1,11 @@
+﻿using NatCruise.Services;
+
+namespace NatCruise.Wpf.Services
+{
+    public interface IWpfApplicationSettingService : IApplicationSettingService
+    {
+        string LastOpenCruiseDir { get; set; }
+
+        string LastOpenTemplateDir { get; set; }
+    }
+}
