4577e91beacafeecbe7cca43002206efed4963b8
Ben Campbell <benjocamps@gmail.com>
2021-12-29 21:58:28 -0500

    display validation status in unit, stratum and sg list pages

--- src/NatCruise.Design/ViewModels/SampleGroupListViewModel.cs
+++ src/NatCruise.Design/ViewModels/SampleGroupListViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using NatCruise.Data;
 using NatCruise.Design.Data;
 using NatCruise.Design.Models;
+using NatCruise.Design.Validation;
 using Prism.Commands;
 using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows.Input;
@@ -23,9 +25,12 @@ namespace NatCruise.Design.ViewModels
             var sampleGroupDataservice = datastoreProvider.GetDataservice<ISampleGroupDataservice>();
 
             SampleGroupDataservice = sampleGroupDataservice ?? throw new ArgumentNullException(nameof(sampleGroupDataservice));
+
+            SampleGroupValidator = new SampleGroupValidator();
         }
 
         protected ISampleGroupDataservice SampleGroupDataservice { get; }
+        public SampleGroupValidator SampleGroupValidator { get; }
 
         public ICommand AddSampleGroupCommand => _addSampleGroupCommand ??= new DelegateCommand<string>(AddSampleGroup);
         public ICommand RemoveSampleGroupCommand => _removeSampleGroupCommand ??= new DelegateCommand<SampleGroup>(RemoveSampleGroup);
@@ -43,13 +48,46 @@ namespace NatCruise.Design.ViewModels
         public ObservableCollection<SampleGroup> SampleGroups
         {
             get => _sampleGroups;
-            protected set => SetProperty(ref _sampleGroups, value);
+            protected set
+            {
+                if(_sampleGroups != null)
+                {
+                    foreach(var sg in _sampleGroups)
+                    {
+                        sg.PropertyChanged -= samplegroup_PropertyChanged;
+                    }
+                }
+                SetProperty(ref _sampleGroups, value);
+                if(value != null)
+                {
+                    foreach(var sg in value)
+                    {
+                        sg.PropertyChanged += samplegroup_PropertyChanged;
+                    }
+                }
+            }
+        }
+
+        private void samplegroup_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if(e.PropertyName == nameof(SampleGroup.Errors)) { return; }
+            if(sender is SampleGroup sg && sg != null)
+            {
+                ValidateSampeleGroup(sg);
+            }
         }
 
         public SampleGroup SelectedSampleGroup
         {
             get => _selectedSampleGroup;
-            set => SetProperty(ref _selectedSampleGroup, value);
+            set
+            {
+                SetProperty(ref _selectedSampleGroup, value);
+                if(value != null)
+                {
+                    ValidateSampeleGroup(value);
+                }
+            }
         }
 
         public void AddSampleGroup(string code)
@@ -67,6 +105,7 @@ namespace NatCruise.Design.ViewModels
             };
 
             SampleGroupDataservice.AddSampleGroup(newSampleGroup);
+            newSampleGroup.PropertyChanged += samplegroup_PropertyChanged;
             SampleGroups.Add(newSampleGroup);
             SelectedSampleGroup = newSampleGroup;
         }
@@ -79,6 +118,7 @@ namespace NatCruise.Design.ViewModels
             SampleGroupDataservice.DeleteSampleGroup(sampleGroup);
             var index = sampleGroups.IndexOf(sampleGroup);
             sampleGroups.Remove(sampleGroup);
+            sampleGroup.PropertyChanged -= samplegroup_PropertyChanged;
 
             if (index < 0) { return; }
             if (index <= sampleGroups.Count - 1)
@@ -96,7 +136,16 @@ namespace NatCruise.Design.ViewModels
         {
             if (stratum != null)
             {
-                SampleGroups = new ObservableCollection<SampleGroup>(SampleGroupDataservice.GetSampleGroups(stratum.StratumCode));
+                var sampleGroups = SampleGroupDataservice.GetSampleGroups(stratum.StratumCode).ToArray();
+
+                foreach (var sampleGroup in sampleGroups)
+                {
+                    ValidateSampeleGroup(sampleGroup);
+                }
+
+                SampleGroups = new ObservableCollection<SampleGroup>(sampleGroups);
+
+                
             }
         }
 
@@ -105,6 +154,17 @@ namespace NatCruise.Design.ViewModels
             base.Load();
 
             LoadSampleGroups(Stratum);
+        }
+
+        public void ValidateSampeleGroup(SampleGroup sampleGroup)
+        {
+            if (sampleGroup is null) { throw new ArgumentNullException(nameof(sampleGroup)); }
+
+            var errors = SampleGroupValidator.Validate(sampleGroup).Errors
+                .Where(x => x.Severity == FluentValidation.Severity.Error)
+                .Select(x => x.ErrorMessage).ToArray();
+
+            sampleGroup.Errors = errors;
         }
     }
 }
---
+++ src/NatCruise.Wpf/Converters/IsEmptyConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NatCruise.Wpf.Converters
+{
+    public class IsEmptyConverter : IValueConverter
+    {
+        public bool Invert { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var v = value as IEnumerable;
+
+            if (v != null && v.GetEnumerator().MoveNext())
+            {
+                return false ^ Invert;
+            }
+            return true ^ Invert;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
