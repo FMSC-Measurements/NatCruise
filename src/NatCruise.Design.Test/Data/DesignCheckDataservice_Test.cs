b8950c800402af01f398ff0ea527fd46d9cbb53a
Benjamin J.Campbell <benjamin.campbell@usda.gov>
2022-01-12 15:37:12 -0500

    feature add design checks

---
+++ src/NatCruise.Design.Test/Data/DesignCheckDataservice_Test.cs
@@ -0,0 +1,178 @@
+﻿using CruiseDAL.V3.Models;
+using FluentAssertions;
+using NatCruise.Design.Data;
+using NatCruise.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace NatCruise.Design.Test.Data
+{
+    public class DesignCheckDataservice_Test : TestBase
+    {
+        public DesignCheckDataservice_Test(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void GetSubPopTDVChecks_Test()
+        {
+            var init = new DatastoreInitializer();
+
+            var strata = init.Strata = new[]
+            {
+                new Stratum{ StratumCode = "st1", Method = "STR" },
+            };
+
+            init.UnitStrata = new[]
+            {
+                new CuttingUnit_Stratum {CuttingUnitCode = init.Units[0], StratumCode = strata[0].StratumCode },
+            };
+
+            var sampleGroups = init.SampleGroups = new[]
+            {
+                new SampleGroup {SampleGroupCode = "sg1", StratumCode = strata[0].StratumCode, SamplingFrequency = 101, TallyBySubPop = true},
+            };
+
+            var species = init.Species;
+
+            var tdvs = init.TreeDefaults = new[]
+            {
+                new TreeDefaultValue {SpeciesCode = species[0], PrimaryProduct = "01"},
+            };
+
+            var subPops = init.Subpops = new[]
+            {
+                    new SubPopulation {
+                    StratumCode = sampleGroups[0].StratumCode,
+                    SampleGroupCode = sampleGroups[0].SampleGroupCode,
+                    SpeciesCode = species[0],
+                    LiveDead = "L",
+                },
+            };
+
+            using var db = init.CreateDatabase();
+
+            var ds = new DesignCheckDataservice(db, init.CruiseID, init.DeviceID);
+
+            var result = ds.GetSubPopTDVChecks();
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void GetSubPopTDVChecks_Test_WithCatchAll()
+        {
+            var init = new DatastoreInitializer();
+
+            var strata = init.Strata = new[]
+            {
+                new Stratum{ StratumCode = "st1", Method = "STR" },
+            };
+
+            init.UnitStrata = new[]
+            {
+                new CuttingUnit_Stratum {CuttingUnitCode = init.Units[0], StratumCode = strata[0].StratumCode },
+            };
+
+            var sampleGroups = init.SampleGroups = new[]
+            {
+                new SampleGroup {SampleGroupCode = "sg1", StratumCode = strata[0].StratumCode, SamplingFrequency = 101, PrimaryProduct = "01", TallyBySubPop = true},
+            };
+
+            var species = init.Species;
+
+            var tdvs = init.TreeDefaults = new[]
+            {
+                new TreeDefaultValue {SpeciesCode = species[0], PrimaryProduct = "01"},
+                new TreeDefaultValue {SpeciesCode = null, PrimaryProduct = null},
+            };
+
+            var subPops = init.Subpops = new[]
+            {
+                    new SubPopulation
+                    {
+                        StratumCode = sampleGroups[0].StratumCode,
+                        SampleGroupCode = sampleGroups[0].SampleGroupCode,
+                        SpeciesCode = species[0],
+                        LiveDead = "L",
+                    },
+                    new SubPopulation
+                    {
+                        StratumCode = sampleGroups[0].StratumCode,
+                        SampleGroupCode = sampleGroups[0].SampleGroupCode,
+                        SpeciesCode = species[1],
+                        LiveDead = "L",
+                    },
+            };
+
+
+            using var db = init.CreateDatabase();
+
+            var ds = new DesignCheckDataservice(db, init.CruiseID, init.DeviceID);
+
+            var result = ds.GetSubPopTDVChecks();
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void GetSubPopTDVChecks_Test_MissingTDV()
+        {
+            var init = new DatastoreInitializer();
+
+            var strata = init.Strata = new[]
+            {
+                new Stratum{ StratumCode = "st1", Method = "STR" },
+            };
+
+            init.UnitStrata = new[]
+            {
+                new CuttingUnit_Stratum {CuttingUnitCode = init.Units[0], StratumCode = strata[0].StratumCode },
+            };
+
+            var sampleGroups = init.SampleGroups = new[]
+            {
+                new SampleGroup {SampleGroupCode = "sg1", StratumCode = strata[0].StratumCode, SamplingFrequency = 101, TallyBySubPop = true},
+            };
+
+            var species = init.Species;
+
+            var tdvs = init.TreeDefaults = new[]
+            {
+                new TreeDefaultValue {SpeciesCode = species[0], PrimaryProduct = "01"},
+            };
+
+            var subPops = init.Subpops = new[]
+            {
+                    new SubPopulation
+                    {
+                        StratumCode = sampleGroups[0].StratumCode,
+                        SampleGroupCode = sampleGroups[0].SampleGroupCode,
+                        SpeciesCode = species[0],
+                        LiveDead = "L",
+                    },
+                    new SubPopulation
+                    {
+                        StratumCode = sampleGroups[0].StratumCode,
+                        SampleGroupCode = sampleGroups[0].SampleGroupCode,
+                        SpeciesCode = species[1],
+                        LiveDead = "L",
+                    },
+            };
+
+
+            using var db = init.CreateDatabase();
+
+            var ds = new DesignCheckDataservice(db, init.CruiseID, init.DeviceID);
+
+            var results = ds.GetSubPopTDVChecks();
+            results.Should().HaveCount(1);
+
+            var result = results.First();
+            Output.WriteLine(result.Message);
+        }
+    }
+}
