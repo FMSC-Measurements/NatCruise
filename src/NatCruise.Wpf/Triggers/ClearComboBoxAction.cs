5b4081f14d5820cce44c94c57f58173ffdf039f4
Benjamin J.Campbell <benjamin.campbell@usda.gov>
2022-01-21 15:27:11 -0500

    update MahApps.Metro to 2.4.9
    Also added package reference to Microsoft.Xaml.Behavior.Wpf. This was needed because we changed TargetFramework to net60, however because we were using an older version of MahApps.Metro we were being forced back to net 460

--- src/NatCruise.Wpf/Triggers/ClearComboBoxAction.cs
+++ src/NatCruise.Wpf/Triggers/ClearComboBoxAction.cs
@@ -1,10 +1,11 @@
-﻿using System.Windows.Controls;
-using System.Windows.Interactivity;
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows.Controls;
 
 namespace NatCruise.Wpf.Triggers
 {
     public class ClearComboBoxAction : TargetedTriggerAction<ComboBox>
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>")]
         protected override void Invoke(object parameter)
         {
             var target = Target;
