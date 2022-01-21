81b657df5a2652de7f380999b5e7d2cb9044307e
Ben Campbell <benjocamps@gmail.com>
2022-01-21 15:27:11 -0500

    update MahApps.Metro to 2.4.9
    Also added package reference to Microsoft.Xaml.Behavior.Wpf. This was needed because we changed TargetFramework to net60, however because we were using an older version of MahApps.Metro we were being forced back to net 460

--- src/NatCruise.Wpf/Triggers/ClearTextBoxAction.cs
+++ src/NatCruise.Wpf/Triggers/ClearTextBoxAction.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿using Microsoft.Xaml.Behaviors;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
-using System.Windows.Interactivity;
 
 namespace NatCruise.Wpf.Triggers
 {
     public class ClearTextBoxAction : TargetedTriggerAction<TextBox>
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>")]
         protected override void Invoke(object parameter)
         {
             var target = Target;
