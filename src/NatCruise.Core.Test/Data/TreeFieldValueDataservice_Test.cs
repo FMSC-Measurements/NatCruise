b88d0446418dfd7a6eb8fab2ef053575e3e51054
Ben Campbell <benjocamps@gmail.com>
2022-05-05 13:54:33 -0400

    Add TreeFieldValueDataservice

---
+++ src/NatCruise.Core.Test/Data/TreeFieldValueDataservice_Test.cs
@@ -0,0 +1,103 @@
+﻿using CruiseDAL.V3.Models;
+using FluentAssertions;
+using NatCruise.Data;
+using System;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace NatCruise.Test.Data
+{
+    public class TreeFieldValueDataservice_Test : TestBase
+    {
+        public TreeFieldValueDataservice_Test(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void GetTreeFieldValues_CheckFieldOrder()
+        {
+            var init = new DatastoreInitializer();
+            using var db = init.CreateDatabase();
+
+            var cruiseID = init.CruiseID;
+            var stCode = "st1";
+            var treeFieldSetups = new[]
+            {
+                new TreeFieldSetup
+                {
+                    CruiseID = cruiseID,
+                    StratumCode = stCode,
+                    Field = nameof(TreeMeasurment.DBH),
+                },
+                new TreeFieldSetup
+                {
+                    CruiseID = cruiseID,
+                    StratumCode = stCode,
+                    Field = nameof(TreeMeasurment.Initials),
+                },
+                new TreeFieldSetup
+                {
+                    CruiseID = cruiseID,
+                    StratumCode = stCode,
+                    Field = nameof(TreeMeasurment.DRC),
+                }
+            };
+
+            // add fields in reverse order to insure insertion order doesn't effect
+            // order TFV come out
+            foreach (var (tfs, i) in treeFieldSetups.Select((x, i) => (x, i)).Reverse())
+            {
+                tfs.FieldOrder = i;
+                db.Insert(tfs);
+            }
+
+            var treeID = Guid.NewGuid().ToString();
+            var tree = new Tree
+            {
+                CruiseID = cruiseID,
+                TreeID = treeID,
+                TreeNumber = 1,
+                CuttingUnitCode = "u1",
+                StratumCode = stCode,
+                SampleGroupCode = "sg1",
+            };
+            db.Insert(tree);
+
+            var ds = new TreeFieldValueDataservice(db, cruiseID, "devID");
+
+            var tfvs = ds.GetTreeFieldValues(treeID);
+            tfvs.Should().HaveSameCount(treeFieldSetups);
+
+            var tfvFields = tfvs.Select(x => x.Field.ToLower());
+            var tfsFields = treeFieldSetups.Select(x => x.Field.ToLower());
+            tfvFields.Should().BeEquivalentTo(tfsFields);
+        }
+
+        //[Fact]
+        //public void Issue_TreeFieldsNotInCorrectOrder()
+        //{
+        //    var testFile = GetTestFile("Issue_TreeFieldsNotInCorrectOrder.crz3db");
+
+        //    var db = new CruiseDatastore_V3(testFile);
+
+        //    var cruiseID = db.From<Cruise>().Query().Single().CruiseID;
+
+        //    var tree = db.From<Tree>().Query().Single();
+        //    var ds = new TreeFieldValueDataservice(db, cruiseID, "devID");
+
+        //    var tfs = db.From<TreeFieldSetup>().Where("StratumCode = @p1 AND SampleGroupCode IS NULL")
+        //        .OrderBy("FieldOrder")
+        //        .Query(tree.StratumCode).ToArray();
+
+        //    var tfvs = ds.GetTreeFieldValues(tree.TreeID);
+
+        //    tfvs.Should().HaveSameCount(tfs);
+
+        //    var tfsFields = tfs.Select(x => x.Field.ToLower());
+        //    var tfvFields = tfvs.Select(y => y.Field.ToLower());
+
+        //    tfvFields.Should().BeEquivalentTo(tfsFields);
+        //}
+    }
+}
---
+++ src/NatCruise.Core/Data/TreeFieldValueDataservice.cs
@@ -0,0 +1,59 @@
+﻿using CruiseDAL;
+using NatCruise.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NatCruise.Data
+{
+    public class TreeFieldValueDataservice : CruiseDataserviceBase, ITreeFieldValueDataservice
+    {
+        public TreeFieldValueDataservice(CruiseDatastore_V3 database, string cruiseID, string deviceID) : base(database, cruiseID, deviceID)
+        {
+        }
+
+        public TreeFieldValueDataservice(string path, string cruiseID, string deviceID) : base(path, cruiseID, deviceID)
+        {
+        }
+
+        public IEnumerable<TreeFieldValue> GetTreeFieldValues(string treeID)
+        {
+            return Database.Query<TreeFieldValue>(
+                "SELECT " +
+                    "t.TreeID, " +
+                    "tfs.Field, " +
+                    "ifnull(tfh.Heading, tf.DefaultHeading) AS Heading, " +
+                    "tf.DbType, " +
+                    "tfv.ValueReal, " +
+                    "tfv.ValueBool, " +
+                    "tfv.ValueText, " +
+                    "tfv.ValueInt, " +
+                    "tfs.DefaultValueInt, " +
+                    "tfs.DefaultValueReal, " +
+                    "tfs.DefaultValueBool, " +
+                    "tfs.DefaultValueText, " +
+                    "tfs.IsHidden, " +
+                    "tfs.IsLocked " +
+                "FROM Tree AS t " +
+                "JOIN TreeFieldSetup AS tfs ON t.StratumCode = tfs.StratumCode AND t.CruiseID = tfs.CruiseID AND (tfs.SampleGroupCode IS NULL OR t.SampleGroupCode = tfs.SampleGroupCode) " +
+                "LEFT JOIN TreeFieldHeading AS tfh USING (Field, CruiseID) " +
+                "JOIN TreeField AS tf USING (Field) " +
+                "LEFT JOIN TreeFieldValue_All AS tfv USING (TreeID, Field) " +
+                "WHERE t.TreeID = @p1 " +
+                "ORDER BY tfs.FieldOrder;", treeID).ToArray();
+        }
+
+        public void UpdateTreeFieldValue(TreeFieldValue treeFieldValue)
+        {
+            if (treeFieldValue is null) { throw new ArgumentNullException(nameof(treeFieldValue)); }
+
+            Database.Execute(
+                "INSERT INTO TreeMeasurment " +
+                $"(TreeID, {treeFieldValue.Field}, CreatedBy)" +
+                $"VALUES (@p1, @p2, @p3)" +
+                $"ON CONFLICT (TreeID) DO " +
+                $"UPDATE SET {treeFieldValue.Field} = @p2, ModifiedBy = @p3 WHERE TreeID = @p1;",
+                treeFieldValue.TreeID, treeFieldValue.Value, DeviceID);
+        }
+    }
+}
