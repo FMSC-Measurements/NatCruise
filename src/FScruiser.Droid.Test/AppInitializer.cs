897282962b1a03e1db4f6c43c78d59044c32c228
Benjamin J.Campbell <benjamin.campbell@usda.gov>
2022-06-24 16:29:46 -0400

    update FScruiser.Droid.Test

---
+++ src/FScruiser.Droid.Test/AppInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace FScruiser.Droid.Test
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                //Environment.SetEnvironmentVariable("ANDROID_HOME", "%ProgramFiles(x86)%\\Android\\android-sdk", EnvironmentVariableTarget.Process);
+                //Environment.SetEnvironmentVariable("JAVA_HOME", "%ProgramFiles%\Android\Jdk\microsoft_dist_openjdk_1.8.0.25");
+
+                var keystorePath = Path.GetFullPath("../../../../FScruiser.Droid/FMSC.keystore");
+                var keystorePW = Environment.GetEnvironmentVariable("FScruiserKeystorePass");
+
+                return ConfigureApp
+                .Android
+                .EnableLocalScreenshots()
+                //.KeyStore(keystorePath, keystorePW, keystorePW, keyAlias: "FMSC") // if testing a signed release version the test runner needs to be signed with the same keystore
+                .InstalledApp("com.FMSC.FScruiser")
+                //.ApkFile("../../../FScruiser/FScruiser.Droid/bin/Release/FScruiser.Droid.apk")
+                .StartApp();
+
+            }
+            else
+            {
+                throw new NotSupportedException();
+                //return ConfigureApp.iOS.StartApp();
+            }
+        }
+    }
+}
