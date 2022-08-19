a51b50eeaae723efd402743a442e9b911505c030
Benjamin J.Campbell <benjamin.campbell@usda.gov>
2022-08-19 14:51:06 -0500

    WIP update limiting distance calculations

--- src/NatCruise.Cruise/Logic/CalculateLimitingDistance.cs
+++ src/NatCruise.Cruise/Logic/CalculateLimitingDistance.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NatCruise.Util;
+using System;
 
 namespace NatCruise.Cruise.Logic
 {
@@ -46,6 +47,92 @@ namespace NatCruise.Cruise.Logic
                 double limitingDistance = (plotRad - toFaceCorrection) * slopeCorrectionFactor;
                 return limitingDistance;
             }
+        }
+
+        public static decimal Calculate2(decimal bafORfps, decimal dbh,
+            int slopPct, bool isVariableRadius, bool isToFace)
+        {
+            dbh = Math.Round(dbh, 1, MidpointRounding.AwayFromZero); // dbh should be rounded to tenth of an inch
+            if (dbh <= 0.0m
+                || bafORfps <= 0) { return 0.0m; }
+
+            decimal toFaceCorrection = (isToFace) ?
+                (dbh / (12.0m * 2)) // to calculate to face correction: Half DBH in feet (divided by 12) 
+                : 0.0m;
+
+            decimal slope = slopPct / 100.0m; // convert slope percentage to decimal value
+
+            if (isVariableRadius)
+            {
+                // Reference: FSH 2409.12 35.22a
+                decimal plotRadiusFactor = (8.696m / DecimalMath.Sqrt(bafORfps));
+                decimal limitingDistance = dbh * plotRadiusFactor;
+                decimal correctedPRF = (limitingDistance - toFaceCorrection) / dbh;
+
+                
+
+                decimal slopeCorrectionFactor = 0m;
+                if(slopPct >= 10)
+                {
+                    slopeCorrectionFactor = DecimalMath.Sqrt(1.0m + (slope * slope));
+                }
+                decimal correctedLimitingDistance = dbh * correctedPRF * slopeCorrectionFactor;
+                return correctedLimitingDistance;
+            }
+            else
+            {
+                // Reference: FSH 2409.12 34.22
+                decimal plotRad = DecimalMath.Sqrt((43560 / bafORfps) / DecimalMath.Pi);
+                decimal slopeCorrectionFactor = 1 / DecimalMath.Cos(DecimalMath.ATan(slope));
+                decimal limitingDistance = (plotRad - toFaceCorrection) * slopeCorrectionFactor;
+                return limitingDistance;
+            }
+        }
+
+        public static decimal CalculateVariableRadious(decimal baf, decimal dbh, int slopePct, bool isToFace)
+        {
+            dbh = Math.Round(dbh, 1, MidpointRounding.AwayFromZero); // dbh should be rounded to tenth of an inch
+            if (dbh <= 0.0m
+                || baf <= 0) { return 0.0m; }
+
+
+            // Reference: FSH 2409.12 35.22a
+            decimal plotRadiusFactor = (8.696m / DecimalMath.Sqrt(baf));
+            decimal limitingDistance = dbh * plotRadiusFactor;
+
+            if(isToFace)
+            {
+                decimal toFaceCorrection = CalculateToFaceCorrection(dbh);
+                limitingDistance = limitingDistance - toFaceCorrection;
+            }
+
+            if (slopePct >= 10) // slope correction factor should only be calculated for slopes 10% or more
+            {
+                decimal slope = slopePct / 100.0m; // convert slope percentage to decimal value
+                decimal slopeCorrectionFactor = DecimalMath.Sqrt(1.0m + (slope * slope)); // round to two decimals?
+
+                limitingDistance = limitingDistance * slopeCorrectionFactor;
+            }
+
+            return limitingDistance;
+        }
+
+        public static decimal CalculateToFaceCorrection(decimal dbh)
+        {
+            return (dbh / (12.0m * 2)); // to calculate to face correction: Half DBH in feet (divided by 12) 
+        }
+
+        public static decimal CalculateToSlopeCorrectionFactor(int slopePct)
+        {
+            decimal slope = slopePct / 100.0m; // convert slope percentage to decimal value
+            return DecimalMath.Sqrt(1.0m + (slope * slope)); // round to two decimals?
+        }
+
+        public static decimal CalculateFixSize(decimal fps, decimal dbh, int slopePct, bool isToFace)
+        {
+            dbh = Math.Round(dbh, 1, MidpointRounding.AwayFromZero); // dbh should be rounded to tenth of an inch
+            if (dbh <= 0.0m
+                || baf <= 0) { return 0.0m; }
         }
 
         public static bool DeterminTreeInOrOut(double slopeDistance, double limitingDistance)
