2c8d81612b0ebff0c3dd228ca6d7e81d3730e4a9
Benjamin J.Campbell <benjamin.campbell@usda.gov>
2022-09-09 16:03:59 -0500

    ENHANCMENT don't allow non numeric input into numeric textboxes

---
+++ src/NatCruise.Wpf/Controls/TextBoxHelper.cs
@@ -0,0 +1,161 @@
+﻿using ControlzEx.Standard;
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace NatCruise.Wpf.Controls
+{
+    public class TextBoxHelper
+    {
+        public static readonly DependencyProperty RegexMaskProperty = DependencyProperty.RegisterAttached(
+            "RegexMask",
+            typeof(string),
+            typeof(TextBoxHelper),
+            new UIPropertyMetadata(OnRegexMaskChanged)
+            );
+
+        public static string GetRegexMask(DependencyObject obj)
+        {
+            return (string)obj.GetValue(RegexMaskProperty);
+        }
+
+        public static void SetRegexMask(DependencyObject obj, string value)
+        {
+            obj.SetValue(RegexMaskProperty, value);
+        }
+
+        public static void OnRegexMaskChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            var textbox = obj as TextBox;
+            if (textbox == null) return;
+
+            textbox.PreviewTextInput += TextBox_PreviewTextInput;
+            DataObject.AddPastingHandler(textbox, TextBox_Pasting);
+        }
+
+        private static void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // note: previewTextInput event is not called if Space, backspace, Delete are pressed
+
+
+            var textbox = sender as TextBox;
+
+            var mask = GetRegexMask(textbox);
+            if (mask == null) return;
+            e.Handled = true;
+
+            var text = textbox.Text;
+            var position = textbox.CaretIndex;
+            //var nextPosition = position;
+            var input = e.Text;
+            var inputLen = input.Length;
+
+            var selectionLength = textbox.SelectionLength;
+            if (selectionLength > 0)
+            {
+                var selectionStart = textbox.SelectionStart;
+                var selectionEnd = selectionStart + selectionLength;
+                if (selectionLength == text.Length)
+                {
+                    text = input;
+                }
+                else if (selectionStart == 0)
+                {
+                    text = input + text.Substring(selectionEnd);
+                }
+                else
+                {
+                    text = text.Substring(0, selectionStart) + input + text.Substring(selectionEnd);
+                }
+            }
+            else if (Keyboard.IsKeyToggled(Key.Insert))
+            {
+                // calculate length of text after adding edits
+                var textLength = Math.Max(position + inputLen, text.Length);
+
+                // copy existing text over to char array 
+                var chars = new char[textLength];
+                text.CopyTo(0, chars, 0, text.Length);
+
+                // copy over inserted text to char arry
+                input.CopyTo(0, chars, position, inputLen);
+
+                text = new String(chars);
+            }
+            else
+            {
+                text = text.Insert(position, input);
+            }
+
+            // test new text value against our Regex mask
+            var match = Regex.Match(text, mask);
+            // if there is a match and all the text is part of the match
+            // it might be possible to have multiple matches but we aren't allowing that
+            if (match != null && match.Length == text.Length)
+            {
+                textbox.SetValue(TextBox.TextProperty, text);
+
+                // advance cursor
+                var nextPosition = position + inputLen;
+                textbox.CaretIndex = nextPosition;
+
+            }
+            else
+            {
+                System.Media.SystemSounds.Beep.Play();
+                return;
+            }
+        }
+
+        private static void TextBox_Pasting(object? sender, DataObjectPastingEventArgs e)
+        {
+            var textbox = sender as TextBox;
+
+            // if is string and not drag and drop
+            if (e.DataObject.GetDataPresent(typeof(string)) && !e.IsDragDrop)
+            {
+                var pastedText = (string)e.DataObject.GetData(typeof(string));
+
+                var text = textbox.Text;
+                var position = textbox.CaretIndex;
+                var inputLen = pastedText.Length;
+
+                text = text.Insert(position, pastedText);
+
+                var mask = GetRegexMask(textbox);
+                if (mask == null) return;
+                // test new text value against our Regex mask
+                var match = Regex.Match(text, mask);
+                // if there is a match and all the text is part of the match
+                // it might be possible to have multiple matches but we aren't allowing that
+                if (match != null && match.Length == text.Length)
+                {
+                    textbox.SetValue(TextBox.TextProperty, text);
+
+                    // advance cursor
+                    var nextPosition = position + inputLen;
+                    textbox.CaretIndex = nextPosition;
+
+                    textbox.Focus();
+
+                }
+                else
+                {
+                    System.Media.SystemSounds.Beep.Play();
+                }
+
+                e.Handled = true;
+            }
+            else
+            {
+                // cancel drag and drop event
+                e.CancelCommand();
+            }
+            
+        }
+    }
+}
---
+++ src/NatCruise.Wpf/Validation/RegexValidationRule.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace NatCruise.Wpf.Validation
+{
+    public class RegexValidationRule : ValidationRule
+    {
+        public RegexValidationRule()
+        {
+            base.ValidationStep = ValidationStep.RawProposedValue;
+        }
+
+        public string Match { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var sValue = (string)value;
+
+            if (Regex.IsMatch(sValue, Match))
+            {
+                return ValidationResult.ValidResult;
+            }
+            else
+            {
+                return new ValidationResult(false, "Invalid Value");
+            }
+        }
+    }
+}
