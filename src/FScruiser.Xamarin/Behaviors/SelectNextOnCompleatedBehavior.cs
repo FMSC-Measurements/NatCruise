dcc6d92a613d05a6f4b229db240f6fceeabc38d8
Benjamin J.Campbell <benjamin.campbell@usda.gov>
2022-12-23 12:45:44 -0600

    [#45][FScruiser] fix clicking done on last tree field doesn't move to Remarks field

--- src/FScruiser.Xamarin/Behaviors/SelectNextOnCompleatedBehavior.cs
+++ src/FScruiser.Xamarin/Behaviors/SelectNextOnCompleatedBehavior.cs
@@ -10,6 +10,19 @@ namespace FScruiser.XF.Behaviors
         private const int MAX_SEARCH_DEPTH = 10;
         public Type ParentType { get; set; } = typeof(StackLayout);
 
+        public static readonly BindableProperty ParentNextElementProperty =
+            BindableProperty.Create(
+                "ParentNextElement",
+                typeof(VisualElement),
+                typeof(SelectNextOnCompleatedBehavior),
+                default(VisualElement));
+
+        public VisualElement ParentNextElement
+        {
+            get => (VisualElement)GetValue(ParentNextElementProperty);
+            set => SetValue(ParentNextElementProperty, value);
+        }
+
         protected override void OnAttachedTo(Entry entry)
         {
             base.OnAttachedTo(entry);
@@ -21,14 +34,23 @@ namespace FScruiser.XF.Behaviors
         {
             var entry = (Entry)sender;
 
-            var ancestor = FindAncestor(entry, ParentType, out var lastChild) as Layout;
+            var ancestor = FindAncestor(entry, ParentType, out var currChild) as Layout;
             if (ancestor == null) return;
             var ancestorChildren = ancestor.Children;
-            var lastChildIndex = ancestorChildren.IndexOf(lastChild);
+            var currChildIndex = ancestorChildren.IndexOf(currChild);
 
-            if (lastChildIndex >= ancestorChildren.Count - 1) return;
+            if (currChildIndex >= ancestorChildren.Count - 1)
+            {
+                var parentNextElement = ParentNextElement;
+                if (parentNextElement != null)
+                {
+                    parentNextElement.Focus();
+                    return;
+                }
+                return;
+            }
 
-            var nextChildDownTheLine = ancestorChildren[lastChildIndex + 1] as VisualElement;
+            var nextChildDownTheLine = ancestorChildren[currChildIndex + 1] as VisualElement;
             if (nextChildDownTheLine == null) return;
 
             var nextInput = FindNextInputOrPicker(nextChildDownTheLine);
