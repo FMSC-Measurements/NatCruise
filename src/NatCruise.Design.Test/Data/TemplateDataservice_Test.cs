0d83eba71eea2fd1f3ef15b98c8c1a7719926fc0
Benjamin J. Campbell <benjamin.campbell@usda.gov>
2023-04-21 20:01:14 -0500

    clean up TemplateDataservice_Test

--- src/NatCruise.Design.Test/Data/TemplateDataservice_Test.cs
+++ src/NatCruise.Design.Test/Data/TemplateDataservice_Test.cs
@@ -16,67 +16,6 @@ namespace NatCruise.Design.Test.Data
         {
         }
 
-        #region species
-
-        [Fact]
-        public void AddSpecies()
-        {
-            var speciesCode = "ABC";
-            var initializer = new DatastoreInitializer();
-            using var db = initializer.CreateDatabase();
-            var ds = new TemplateDataservice(db, initializer.CruiseID, initializer.DeviceID);
-
-            var sp = new Species
-            {
-                SpeciesCode = speciesCode,
-            };
-
-            ds.AddSpecies(sp);
-
-            db.ExecuteScalar<int>("SELECT count(*) FROM Species WHERE SpeciesCode = @p1", speciesCode)
-                .Should().Be(1);
-        }
-
-        [Fact]
-        public void GetSpecies()
-        {
-            var initializer = new DatastoreInitializer();
-            using var db = initializer.CreateDatabase();
-            var ds = new TemplateDataservice(db, initializer.CruiseID, initializer.DeviceID);
-
-            var species = ds.GetSpecies();
-            species.Should().NotBeEmpty();
-        }
-
-        [Fact]
-        public void UpsertSpecies_Insert()
-        {
-            var speciesCode = "ABC";
-            var initializer = new DatastoreInitializer();
-            using var db = initializer.CreateDatabase();
-            var ds = new TemplateDataservice(db, initializer.CruiseID, initializer.DeviceID);
-
-            var sp = new Species
-            {
-                SpeciesCode = speciesCode,
-            };
-
-            ds.UpsertSpecies(sp);
-
-            db.ExecuteScalar<int>("SELECT count(*) FROM Species WHERE SpeciesCode = @p1", speciesCode)
-                .Should().Be(1);
-
-            sp.ContractSpecies = Rand.Word();
-            sp.FIACode = Rand.Word();
-
-            ds.UpsertSpecies(sp);
-
-            var spAgain = db.From<Species>().Where("SpeciesCode = @p1").Query(speciesCode).Single();
-            spAgain.Should().BeEquivalentTo(sp);
-        }
-
-        #endregion species
-
         #region StratumTemplate
 
         [Fact]
