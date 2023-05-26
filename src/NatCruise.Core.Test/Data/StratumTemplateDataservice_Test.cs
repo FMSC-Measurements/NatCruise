95bb808f14d40e02fd8385979752effa5b05f0a2
Benjamin J. Campbell <benjamin.campbell@usda.gov>
2023-05-25 23:26:37 -0500

    roll version

---
+++ src/NatCruise.Core.Test/Data/StratumTemplateDataservice_Test.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using NatCruise.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+using StratumTemplate = NatCruise.Models.StratumTemplate;
+using StratumTemplateLogFieldSetup = NatCruise.Models.StratumTemplateLogFieldSetup;
+
+namespace NatCruise.Test.Data
+{
+    public class StratumTemplateDataservice_Test : TestBase
+    {
+        public StratumTemplateDataservice_Test(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void UpsertStratumTemplate()
+        {
+            var initializer = new DatastoreInitializer();
+            using var db = initializer.CreateDatabase();
+            var ds = new StratumTemplateDataservice(db, initializer.CruiseID, initializer.DeviceID);
+
+            var st = new NatCruise.Models.StratumTemplate()
+            {
+                StratumTemplateName = "something",
+            };
+
+            ds.UpsertStratumTemplate(st);
+
+            db.From<StratumTemplate>().Count().Should().Be(1);
+        }
+
+        #region StratumTemplateLogFieldSetup
+
+        [Fact]
+        public void UpsertStratumTemplateLogFieldSetup_Insert()
+        {
+            var initializer = new DatastoreInitializer();
+            using var db = initializer.CreateDatabase();
+            var ds = new StratumTemplateDataservice(db, initializer.CruiseID, initializer.DeviceID);
+
+            var st = new StratumTemplate()
+            {
+                StratumTemplateName = "something",
+            };
+            ds.UpsertStratumTemplate(st);
+
+            var stlfs = new StratumTemplateLogFieldSetup()
+            {
+                Field = "Grade",
+                FieldOrder = 0,
+                StratumTemplateName = st.StratumTemplateName,
+            };
+
+            ds.UpsertStratumTemplateLogFieldSetup(stlfs);
+            db.From<StratumTemplateLogFieldSetup>().Count().Should().Be(1);
+        }
+
+        #endregion StratumTemplateLogFieldSetup
+    }
+}
--- src/NatCruise.Wpf/Setup.iss
+++ src/NatCruise.Wpf/Setup.iss
@@ -1,5 +1,5 @@
 #define MsBuildOutputDir ".\bin\Release\net472"
-#define VERSION "3.1.5.0"
+#define VERSION "3.1.6.0"
 
 #define APP "National Cruise System"
 #define EXEName "NatCruise.Wpf.exe"
