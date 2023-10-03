1858c2246b7c184fa76d60a5bbf994d2252a4a65
Benjamin J. Campbell <benjamin.campbell@usda.gov>
2023-10-03 15:56:23 -0500

    Update NewCruiseViewModel.Test.cs

--- src/NatCruise.Wpf.Test/UnitTests/ViewModels/NewCruiseViewModel.Test.cs
+++ src/NatCruise.Wpf.Test/UnitTests/ViewModels/NewCruiseViewModel.Test.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using CruiseDAL;
+using Moq;
 using NatCruise.Data;
 using NatCruise.Models;
 using NatCruise.Services;
@@ -39,7 +40,7 @@ namespace NatCruise.Wpf.Test.UnitTests.ViewModels
             var cruisePath = GetTempFilePath(".crz3", "CreateCruise_With_V2_Template");
             File.Delete(cruisePath);
 
-            var dsp_moq = new Mock<IDataserviceProvider>();
+            //var dsp_moq = new Mock<IDataserviceProvider>();
             var setupInfo_moq = new Mock<ISetupInfoDataservice>();
             var deviceInfo_moq = new Mock<IDeviceInfoService>();
             deviceInfo_moq.Setup(x => x.DeviceID).Returns(Guid.Empty.ToString());
@@ -47,8 +48,11 @@ namespace NatCruise.Wpf.Test.UnitTests.ViewModels
             var fileDialogService_moq = new Mock<IFileDialogService>();
             fileDialogService_moq.Setup(x => x.SelectCruiseFileDestinationAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
                 .Returns(Task<string>.FromResult(cruisePath));
+            fileDialogService_moq.Setup(x => x.SelectTemplateFileAsync()).ReturnsAsync(templatePath);
 
-            var vm = new NewCruiseViewModel(dsp_moq.Object, setupInfo_moq.Object, fileDialogService_moq.Object, deviceInfo_moq.Object)
+            var dataServiceProvider = new WpfDataserviceProvider((CruiseDatastore_V3)null, deviceInfo_moq.Object);
+
+            var vm = new NewCruiseViewModel(dataServiceProvider, setupInfo_moq.Object, fileDialogService_moq.Object, deviceInfo_moq.Object)
             {
                 SaleNumber = "12345",
                 SaleName = "something",
