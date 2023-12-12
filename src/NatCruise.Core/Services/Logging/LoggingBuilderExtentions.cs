42f6e6f1419c362316bd1f947ff974d597381d3f
Benjamin J. Campbell <benjamin.campbell@usda.gov>
2023-12-12 15:21:29 -0600

    Add appCenterLogger implemented using Microsoft.Extension.Logging

---
+++ src/NatCruise.Core/Services/Logging/LoggingBuilderExtentions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace NatCruise.Services.Logging
+{
+    public static class LoggingBuilderExtentions
+    {
+        public static void AddAppCenterLogger(this ILoggingBuilder builder)
+        {
+            builder.AddProvider(new AppCenterLoggerProvider());
+        }
+    }
+}
