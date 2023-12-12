988937f80f0a4e527a5705d7f880d87e22685012
Benjamin J. Campbell <benjamin.campbell@usda.gov>
2023-12-12 16:01:05 -0600

    add font awesome icons

---
+++ src/FScruiser.Maui/Constants/Fonts.cs
@@ -0,0 +1,13 @@
+﻿namespace FScruiser.Maui.Constants;
+
+public static class Fonts
+{
+    #pragma warning disable VSSpell001 // Spell Check
+    public const string FAsolid = "FA-solid";
+
+    public const string OpenSansRegular = "OpenSansRegular";
+
+
+    public const string OpenSansSemibold = "OpenSansSemibold";
+    #pragma warning restore VSSpell001 // Spell Check
+}
---
+++ src/FScruiser.Maui/Constants/UI.cs
@@ -0,0 +1,7 @@
+﻿namespace FScruiser.Maui.Constants;
+
+public static class UI
+{
+    public const double MinTouchableHeight = 44;
+    public const double MinTouchableWidth = 44;
+}
