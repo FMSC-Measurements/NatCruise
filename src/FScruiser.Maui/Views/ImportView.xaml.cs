7a22b5d8566727d1864b0e8342c45e4108234870
Benjamin J. Campbell <benjamin.campbell@usda.gov>
2024-01-11 16:12:37 -0600

    add view so far

---
+++ src/FScruiser.Maui/Views/ImportView.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FScruiser.Maui.Views;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class ImportView : ContentPage
+{
+    public ImportView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/FScruiser.Maui/Views/SettingsView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FScruiser.Maui.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SettingsView : ContentPage
+	{
+        private bool _courcingSliderValue;
+
+        public SettingsView ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+			if(_courcingSliderValue == true) { return; }
+			try
+			{
+				_courcingSliderValue = true;
+				var slider = (Slider)sender;
+				var value = e.NewValue;
+				slider.Value = Math.Round(value, 1);
+			}
+			finally { _courcingSliderValue = false; }
+        }
+    }
+}
