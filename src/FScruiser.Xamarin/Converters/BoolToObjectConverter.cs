04385386fe332d661dc201188223a1b181f5f692
Benjamin J. Campbell <benjamin.campbell@usda.gov>
2024-01-31 17:14:35 -0600

    ENHANCMENT indicate if stratum exists on plot via add/remove button

---
+++ src/FScruiser.Xamarin/Converters/BoolToObjectConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FScruiser.XF.Converters
+{
+    public class BoolToObjectConverter : IValueConverter
+    {
+        public object TrueValue { get; set; }
+        public object FalseValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is bool b)
+            {
+                if(b)
+                {
+                    return TrueValue;
+                }
+                else
+                {
+                    return FalseValue;
+                }
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
