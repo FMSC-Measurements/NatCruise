0b90fe5d31e8aed7629ae0842981f60b2a7fb3cc
Benjamin J. Campbell <benjamin.campbell@usda.gov>
2024-04-03 17:11:38 -0500

    add cruise design form md doc

---
+++ doc/NatCruiseUserGuide/md_to_docx.cmd
@@ -0,0 +1,23 @@
+::@ECHO OFF
+SETLOCAL ENABLEEXTENSIONS
+
+::Boilderplate 
+::detect if invoked via Window Explorer
+SET interactive=1
+ECHO %CMDCMDLINE% | FIND /I "/c" >NUL 2>&1
+IF %ERRORLEVEL% == 0 SET interactive=0
+
+::name of this script
+SET me=%~n0
+::directory of script
+SET parent=%~dp0
+
+SET pandoc="%parent%\pandoc.cmd"
+
+
+call %pandoc% "%parent%CruiseDesignForm.md" -t docx -o "%parent%CruiseDesignForm.docx" --css "%parent%styles.css" --standalone
+
+::if invoked from windows explorer, pause
+IF "%interactive%"=="0" PAUSE
+ENDLOCAL
+EXIT /B 0
